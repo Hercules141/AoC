60b79750fb30c3594e6fea61a4156b74a70e9e90
Hercules141 <vincentcoby@outlook.de>
2023-12-04 23:58:26 +0100

    setup finished, finally working on desktop pc

---
+++ src/Day01/Day01.kt
@@ -0,0 +1,17 @@
+package Day01
+
+import println
+import readInput
+
+fun main() {
+    fun decode(input: List<String>): Int {
+
+    }
+
+    val testInput = readInput("Day01\\Day01_test")
+    val input = readInput("Day01\\Day01")
+    testInput.println()
+    input.println()
+
+    decode(testInput).println()
+}
